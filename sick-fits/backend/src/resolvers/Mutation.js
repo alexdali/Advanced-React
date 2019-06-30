ced4042de02640bbf3eb9eb6b5245c6d1664cf45
alexeytas <alexdali@gmail.com>
2019-06-30 19:58:07 +0400

    b add first query and mutation for prisma

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,22 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // check if user is logged in
+    // const user = {};
+    // const newargs = {
+    //   ...args,
+    //   user,
+    // };
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args, // ...newargs,
+        },
+      },
+      info
+    );
+
+    return item;
+  },
+};
 
 module.exports = Mutations;
