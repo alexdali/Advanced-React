ef9799db90f32810192af7bcdeb4de94a794888d
alexeytas <alexdali@gmail.com>
2019-07-07 20:56:44 +0400

    f/e. Update Permissions component: add state

--- sick-fits/frontend/components/Permissions.js
+++ sick-fits/frontend/components/Permissions.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
+import PropTypes from 'prop-types';
 import Error from './ErrorMessage';
 import Table from './styles/Table';
 import SickButton from './styles/SickButton';
@@ -45,7 +46,7 @@ const Permissions = props => (
             </thead>
             <tbody>
               {data.users.map(user => (
-                <User user={user} key={user.id} />
+                <UserPermissions user={user} key={user.id} />
               ))}
             </tbody>
           </Table>
@@ -55,9 +56,44 @@ const Permissions = props => (
   </Query>
 );
 
-class User extends Component {
+class UserPermissions extends Component {
+  static propTypes = {
+    user: PropTypes.shape({
+      name: PropTypes.string,
+      email: PropTypes.string,
+      id: PropTypes.string,
+      permissions: PropTypes.array,
+    }).isRequired,
+  };
+
+  state = {
+    permissions: this.props.user.permissions,
+  };
+
+  handlePermissionChange = e => {
+    console.log('et', e.target.checked);
+    console.log('ev', e.target.value);
+    const checkbox = e.target;
+    // take a copy of the current permissions
+    let updatedPermissions = [...this.state.permissions];
+    // figure out it if we need to remove or add this permission
+    if (checkbox.checked) {
+      updatedPermissions.push(checkbox.value);
+    } else {
+      updatedPermissions = updatedPermissions.filter(
+        permission => permission !== checkbox.value
+      );
+    }
+    console.log('new p', updatedPermissions);
+    this.setState({
+      permissions: updatedPermissions,
+    });
+  };
+
   render() {
     const { user } = this.props;
+    // console.log('user state', this.state);
+    console.log(`user ${user.name} state`, this.state);
     return (
       <tr>
         <td>{user.name}</td>
@@ -67,7 +103,9 @@ class User extends Component {
             <label htmlFor={`${user.id}-permission-${permission}`}>
               <input
                 type="checkbox"
-                name={`${user.id}-permission-${permission}`}
+                checked={this.state.permissions.includes(permission)}
+                value={permission}
+                onChange={this.handlePermissionChange}
               />
             </label>
           </td>
