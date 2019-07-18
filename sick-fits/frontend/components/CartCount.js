2549e74ef97a1c55443c444142f010f53fe63882
alexeytas <alexdali@gmail.com>
2019-07-18 15:28:14 +0400

    f/e. Add animation for component CartCount.

--- sick-fits/frontend/components/CartCount.js
+++ sick-fits/frontend/components/CartCount.js
@@ -1,11 +1,36 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
+const AnimationStyles = styled.span`
+  position: relative;
+  .count {
+    display: block;
+    position: relative;
+    transition: all 0.4s;
+    backface-visibility: hidden;
+  }
+  /* Initial State of the entered Dot */
+  .count-enter {
+    /* transform: scale(4) rotate(0.5turn); */
+    transform: scale(4) rotateX(0.5turn);
+  }
+  .count-enter-active {
+    transform: rotateX(0);
+  }
+  .count-exit {
+    top: 0;
+    position: absolute;
+    transform: rotateX(0);
+  }
+  .count-exit-active {
+    transform: scale(4) rotateX(0.5turn);
+  }
+`;
 const Dot = styled.div`
   background: ${props => props.theme.red};
   color: white;
   border-radius: 50%;
-
   padding: 0.5rem;
   line-height: 2rem;
   min-width: 3rem;
@@ -17,7 +42,19 @@ const Dot = styled.div`
 
 const CartCount = ({ count }) => (
   // console.log('CartCount props.cartitem', cartItem);
-  <Dot>{count}</Dot>
+  <AnimationStyles>
+    <TransitionGroup>
+      <CSSTransition
+        unmountOnExit
+        className="count"
+        classNames="count"
+        key={count}
+        timeout={{ enter: 400, exit: 400 }}
+      >
+        <Dot>{count}</Dot>
+      </CSSTransition>
+    </TransitionGroup>
+  </AnimationStyles>
 );
 // CartCount.propTypes = {
 //   count: PropTypes.Int.isRequired,
